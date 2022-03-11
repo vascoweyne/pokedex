221f1855d9b9eea801e6103ea345f6a19a2e035e
Vasco <vasco.weyne@gmail.com>
2022-03-11 18:53:15 -0300

    Features: build first request for pokemonAPI

---
+++ app.js
@@ -0,0 +1,15 @@
+const fetchPokemon = () => {
+  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+  const pokemonPromises = []
+  
+  for(let i = 1; i <= 150; i++){
+    pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+  }
+
+
+  Promise.all(pokemonPromises)
+  .then
+}
+
+fetchPokemon()

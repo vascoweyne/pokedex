2807ef4eb4f375c383491722bb97f5b76b51e034
vascoweyne <vasco.weyne@gmail.com>
2022-03-13 15:29:17 -0300

    create and populate pokemons array and display that in index

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ const fetchPokemon = () => {
     pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
   }
 
-
   Promise.all(pokemonPromises)
-  .then
+    .then(pokemons => {
+      //console.log(pokemons);
+
+    const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+      accumulator += `
+        <li class ="card">
+          <h2 class ="card-title">${pokemon.id}. ${pokemon.name}</h2>
+          <p class ="card-subtitle">${pokemon.types.map(typeInfo => typeInfo.type.name)}</p>
+        </li>
+      `
+      return accumulator
+    }, '')
+    console.log(lisPokemons);
+  })
 }
 
-fetchPokemon()
+fetchPokemon();
